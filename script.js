6ab5725d60000d193a6565038d7a0f0cbc898ef0
MuntjeerAli <muntjeersayyad@gmail.com>
2022-03-21 11:55:43 +0530

    Completed with modal

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 'use strict';
 
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const btn = document.getElementById("btn-modal");
+const start = document.getElementById("start");
 let modal = document.getElementById("modal");
-
-let btn = document.getElementById("btn-modal");
-
 let span = document.getElementsByClassName("close")[0];
+
+const checkEnableButton = () => {
+  btn.disabled = !(
+      firstName.value && 
+      lastName.value && 
+      email.value &&
+      start.value !== 'Choose'
+   )
+}
+firstName.addEventListener('change', checkEnableButton)
+lastName.addEventListener('change', checkEnableButton)
+start.addEventListener('change', checkEnableButton)
+email.addEventListener('change', checkEnableButton)
 
 btn.onclick = function() {
   modal.style.display = "block";
